3291e6bd0bc39f7d1fae61e1401d46701c6e09fc
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2021-03-14 22:47:52 +0100

    Added function to check which status invoices has

--- src/components/Invoice.js
+++ src/components/Invoice.js
@@ -1,12 +1,33 @@
 
 const Invoice = ({ invoice }) => {
 
+    let date = new Date(invoice.paymentDue)
+
+    const checkStatus = status => {
+
+        switch(status) {
+            case 'paid':
+                return 'paid'
+                break;
+            case 'pending':
+                return 'pending'
+                break;
+            case 'draft':
+                return 'draft'
+                break;
+            default:
+                return 'Unknown'
+        }
+    }
+
     return (
         <div className="invoice">
-            <h3>{invoice.id}</h3>
-            <p>{invoice.paymentDue}</p>
-            <p>{invoice.clientName}</p>
-            <h2>{invoice.total}</h2>
+            <h3 className="invoice__id">#<span>{invoice.id}</span></h3>
+            <p className="invoice__date">Due {date.toUTCString().substring(5, 16)}</p>
+            <p className="invoice__name">{invoice.clientName}</p>
+            <h2 className="invoice__price">&#163; {invoice.total}</h2>
+            <button className={`status-btn ${checkStatus(invoice.status)}`}>{invoice.status.charAt(0).toUpperCase() + invoice.status.slice(1)}</button>
+            <span>&#62;</span>
         </div>
     )
 }
