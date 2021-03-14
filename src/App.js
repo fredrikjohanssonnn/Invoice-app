faf8e75e554d67eddcf02f18b0d63f2819513231
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2021-03-14 22:49:27 +0100

    Added props to each component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.scss';
 import Header from './components/Header';
 import InvoiceList from './components/InvoiceList';
 
-function App() {
+const App = () => {
+
+  const [invoices, setInvoices] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8000/data', {
+      headers : { 
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }})
+      .then(res => res.json())
+      .then(data => {
+        setInvoices(data)
+      })
+  }, [])
+
   return (
     <div className="App">
       <div className="container">
-        <Header />
-        <InvoiceList />
+        <Header invoices={invoices}/>
+        <InvoiceList invoices={invoices} />
       </div>
     </div>
   );
