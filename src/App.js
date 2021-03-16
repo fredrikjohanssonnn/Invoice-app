59877720224a80db7ee1d9fc429dc8bd0623c82a
fredrikjohanssonnn <fjohansson.creative@gmail.com>
2021-03-16 22:21:12 +0100

    Added routing with react-router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import { useEffect, useState } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.scss';
 import Header from './components/Header';
+import InvoiceDetails from './components/InvoiceDetails';
 import InvoiceList from './components/InvoiceList';
+import Sidebar from './components/Sidebar';
+import useFetch from './hooks/useFetch';
 
 const App = () => {
 
-  const [invoices, setInvoices] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:8000/data', {
-      headers : { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }})
-      .then(res => res.json())
-      .then(data => {
-        setInvoices(data)
-      })
-  }, [])
+  const { data: invoices } = useFetch('http://localhost:8000/data');
 
   return (
     <div className="App">
+    <Sidebar />
       <div className="container">
-        <Header invoices={invoices}/>
-        <InvoiceList invoices={invoices} />
+        <Switch>
+          <Route exact path="/">
+            <Header invoices={invoices}/>
+            <InvoiceList invoices={invoices} />
+          </Route>
+          <Route path="/invoice/:id">
+            <InvoiceDetails />
+          </Route>
+        </Switch>
       </div>
     </div>
   );
